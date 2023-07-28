b06e7e51318b9798785af3e482c66ba1e2066706
Divyanshu <divyanshu1593@gmail.com>
2023-07-28 13:27:10 +0530

    adding practice code for javascript.info 2.2

---
+++ 2.2/comment.js
@@ -0,0 +1,16 @@
+// a single line comment
+
+/*
+a multiline comment!
+so we can add more than one line in an comment
+*/
+
+// nested comment are NOT supporeted!
+
+// so following code will give error:
+/*
+outer content
+    /*
+    inner content
+    */
+*/
---
+++ 2.2/statement.js
@@ -0,0 +1,21 @@
+// statements in javascript are seperated by ';'
+
+// example:
+
+console.log("first statement");
+console.log("second statement");
+
+// it can work without the semicolon in most casses
+// javascript will interpreat line break as semicolon if it does not generate any error
+// Example:
+console.log("first statement without semicolon")
+console.log("second statement without semicolon")
+
+// if interpreting the line break as semicolon does not from an valid statement than javascript will not do that
+// Example
+console.log(
+    5+
+    5
+);
+
+// not adding semicolon can cause errors in many cases!
