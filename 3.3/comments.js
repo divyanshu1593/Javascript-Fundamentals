59a094ca0e9d251d847d3baf5546858df4061a92
divyanshu <divyanshu1593@gmail.com>
2023-08-02 18:57:28 +0530

    adding practice code for javascript.info 3.3

---
+++ 3.3/comments.js
@@ -0,0 +1,12 @@
+// following comment format should be followed to explain what a function returns and what parameters it takes
+// this is known as JSDoc comment
+
+/**
+ * 
+ * @param {number} length 
+ * @param {number} width 
+ * @returns perimeter of a rectangle
+ */
+function perimeter(length, width){
+    return 2 * (length + width);
+}
