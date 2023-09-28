073dbe253656773ec321134ad848779bb0a9d56b
divyanshu <divyanshu1593@gmail.com>
2023-09-28 17:18:40 +0530

    adding practice code for javascript.info 11.2

---
+++ 11/11.2/task2.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/*
+The built-in function setTimeout uses callbacks. Create a promise-based alternative.
+
+The function delay(ms) should return a promise. That promise should resolve after ms milliseconds, so that we can add .then to it, like this:
+
+function delay(ms) {
+  // your code
+}
+
+delay(3000).then(() => alert('runs after 3 seconds'));
+*/
+
+function delay(ms){
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);
+    });
+}
+
+// checking
+delay(3000).then(() => console.log('runs after 3 seconds'));
